b5dcd2c67882f529aefb57b06773b58eb96fae29
willy <willygodwin47@gmail.com>
2021-03-18 17:16:45 +0800

    Changes file locations

---
+++ routes/api-routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+
+router.get('/api/workouts', (req, res) =>{
+
+
+})
+
+
+router.put('/api/workouts/:id', (req, res) =>{
+
+    
+})
+
+
+router.post('/api/workouts', (req, res) =>{
+
+    
+})
+
+
+router.get('/api/workouts/range', (req, res) =>{
+
+    
+})
+
+
+
+module.exports = router
---
+++ routes/html-routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+router.get('/', (req,res) =>{
+
+
+})
+
+router.get('/stats', (req,res) =>{
+
+    
+})
+
+router.get('/exercise', (req,res) =>{
+
+    
+})
+
+router.get('/exercise?', (req,res) =>{
+
+    
+})
+
